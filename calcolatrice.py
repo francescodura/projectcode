36b71d947b96b56fdb081ce355dae3c73b521859
francescodura <109579085+francescodura@users.noreply.github.com>
2022-07-19 09:55:47 +0200

    a

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,6 +1,10 @@
-from tkinter import *
+#from tkinter import *
 import math
+import sqlite3
+import pandas
 
-root = Tk()
+print(sqlite3.sqlite_version)
+print("ciao")
+# root = Tk()
 
-root.mainloop()
+# root.mainloop()
